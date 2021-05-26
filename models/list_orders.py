c72ae61feaa3344df559e361698dcc8174d662f8
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 19:31:34 +0100

    styling added to header

--- models/list_orders.py
+++ models/list_orders.py
@@ -1,6 +1,7 @@
 from models.order import Order
 from models.customer import Customer
 from models.product import Product
+
 from datetime import date
 
 customer_1 = Customer("Harrison Booth", "hbooth@gmail.com")
@@ -10,6 +11,7 @@ customer_3 = Customer("Gaz Darnell", "gazzaDee@gmail.com")
 product_1 = Product("Pine Grove T-Shirt - Medium")
 product_2 = Product("AC/DC Jumper - Medium")
 product_3 = Product("Foo Fighters, Wasting Light - Vinyl")
+
 
 
 order_001 = Order(customer_1.customer_name, date.today(), 1, product_1.description)
