163d2924a72cd6a3be684b1c7af5a79453d3dce2
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 13:40:16 +0100

    order class and orders setup

---
+++ models/order.py
@@ -0,0 +1,7 @@
+class Order():
+
+    def __init__(self, customer_name, order_date, quantity, merch_description):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.merch_description = merch_description
