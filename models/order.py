2b85a8d23e28c4dce4cf78b1f169bec2ea350e49
OhmniD <dave@ohmnid.com>
2021-05-26 18:42:33 +0100

    add basket and order_line class - add functionality to print all line items in an order and total price

--- models/order.py
+++ models/order.py
@@ -2,9 +2,16 @@ import random
 
 class Order():
 
-    def __init__(self, customer_name, order_date, quantity, merch_description):
+    def __init__(self, customer_name, order_date, basket):
         self.customer_name = customer_name
         self.order_date = order_date
-        self.quantity = quantity
-        self.merch_description = merch_description
+        # self.quantity = quantity
+        # self.merch_description = merch_description
         self.order_number = "GTD" + str(random.randint(1000, 10000))
+        self.basket = basket
+        self.basket_total = 0
+
+        for item in basket:
+            self.basket_total += item.price
+
+        self.basket_total = round(self.basket_total, 2)
--- models/product.py
+++ models/product.py
@@ -1,3 +1,4 @@
 class Product:
-    def __init__(self, description):
+    def __init__(self, description, price):
         self.description = description
+        self.price = price
