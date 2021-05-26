3878e719938da888031dee5d638f6482ea2f9de3
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 15:35:02 +0100

    product class and order number added

--- models/order.py
+++ models/order.py
@@ -1,3 +1,5 @@
+import random
+
 class Order():
 
     def __init__(self, customer_name, order_date, quantity, merch_description):
@@ -5,3 +7,4 @@ class Order():
         self.order_date = order_date
         self.quantity = quantity
         self.merch_description = merch_description
+        self.order_number = "GTD" + str(random.randint(1000, 10000))
