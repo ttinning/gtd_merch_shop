523b5b83fb61238ffbd42bfb769432537307025f
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 14:10:21 +0100

    MVP complete, order module import fixed

--- models/list_orders.py
+++ models/list_orders.py
@@ -1,8 +1,8 @@
-from order import Order
+from models.order import Order
 from datetime import date
 
 order_001 = Order("Harrison Booth", date.today(), 1, "Pine Grove T-Shirt - Medium")
-order_002 = Order("Sandy McMillan", date.today(), 1, "ACDC Jumper - Medium")
+order_002 = Order("Sandy McMillan", date.today(), 1, "AC/DC Jumper - Medium")
 order_003 = Order("Gaz Darnell", date.today(), 1, "Foo Fighters, Wasting Light - Vinyl")
 
 orders = [order_001, order_002, order_003]
