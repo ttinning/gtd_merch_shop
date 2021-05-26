3f072e09f884c60c1d5c6e21493a942585e60e21
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 19:39:36 +0100

    merging files messages removed

--- models/list_orders.py
+++ models/list_orders.py
@@ -1,11 +1,10 @@
 from models.order import Order
 from models.customer import Customer
 from models.product import Product
-<<<<<<< HEAD
 
-=======
+
 from models.order_line import OrderLine
->>>>>>> 9e9bc177834b1be5e8ef9dcc2adb2347bcc187cb
+
 from datetime import date
 
 customer_1 = Customer("Harrison Booth", "hbooth@gmail.com")
@@ -20,12 +19,12 @@ product_4 = Product("DJ Shadow, Endtroducing - Digital Download", 6.99)
 order1_line1 = OrderLine(product_1, 3)
 order1_line2 = OrderLine(product_3, 2)
 
-<<<<<<< HEAD
+
 
 order_001 = Order(customer_1.customer_name, date.today(), 1, product_1.description)
 order_002 = Order(customer_2.customer_name, date.today(), 1, product_2.description)
 order_003 = Order(customer_3.customer_name, date.today(), 1, product_3.description)
-=======
+
 order2_line1 = OrderLine(product_1, 1)
 order2_line2 = OrderLine(product_2, 5)
 
@@ -42,6 +41,6 @@ basket_3 = [order3_line1, order3_line2, order3_line3, order3_line4]
 order_001 = Order(customer_1.customer_name, date.today(), basket_1)
 order_002 = Order(customer_2.customer_name, date.today(), basket_2)
 order_003 = Order(customer_3.customer_name, date.today(), basket_3)
->>>>>>> 9e9bc177834b1be5e8ef9dcc2adb2347bcc187cb
+
 
 orders = [order_001, order_002, order_003]
