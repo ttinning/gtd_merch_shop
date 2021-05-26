125434867cbce300aba80192ea39e4adb79985fb
Tristan Tinning <tristan_tinning@hotmail.com>
2021-05-26 15:23:25 +0100

    customer class added

---
+++ models/customer.py
@@ -0,0 +1,4 @@
+class Customer:
+    def __init__(self, customer_name, e_mail):
+        self.customer_name = customer_name
+        self.e_mail = e_mail
--- models/list_orders.py
+++ models/list_orders.py
@@ -1,8 +1,14 @@
 from models.order import Order
+from models.customer import Customer
 from datetime import date
 
-order_001 = Order("Harrison Booth", date.today(), 1, "Pine Grove T-Shirt - Medium")
-order_002 = Order("Sandy McMillan", date.today(), 1, "AC/DC Jumper - Medium")
-order_003 = Order("Gaz Darnell", date.today(), 1, "Foo Fighters, Wasting Light - Vinyl")
+customer_1 = Customer("Harrison Booth", "hbooth@gmail.com")
+customer_2 = Customer("Sandy McMillan", "smac@gmail.com")
+customer_3 = Customer("Gaz Darnell", "gazzaDee@gmail.com")
+
+
+order_001 = Order(customer_1.customer_name, date.today(), 1, "Pine Grove T-Shirt - Medium")
+order_002 = Order(customer_2.customer_name, date.today(), 1, "AC/DC Jumper - Medium")
+order_003 = Order(customer_3.customer_name, date.today(), 1, "Foo Fighters, Wasting Light - Vinyl")
 
 orders = [order_001, order_002, order_003]
